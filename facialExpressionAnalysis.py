5db3b18fcfb26696ff5a4af0884276c9b5c2534e
Maurício Gabriel Garcia Catellan <mauriciocatellan@gmail.com>
2025-11-07 14:44:45 -0300

    Update Json construction (FER)

--- facialExpressionAnalysis.py
+++ facialExpressionAnalysis.py
@@ -1,28 +1,32 @@
 from deepface import DeepFace
-import json
+import json 
 
 #Real time a ser usado depois na prática:
 #DeepFace.stream(db_path = "/mnt/c/database")           #Acho que já tenho que ter o db criado de antemão
 
-def faceAnalyze(imgName, dataName):
+def faceAnalyze(imgName, dataName, time):
     try:
         objs = DeepFace.analyze(
             img_path = imgName, 
             actions = ['gender', 'race', 'emotion'],
-        )
+        ) 
+        
+        #time é um marcador de timestamp com referencial no próprio vídeo
+        objs.append({'timestamp': time})
         print(objs)
+
         with open(dataName, 'w') as f:
-            json.dump(objs, f)
+            json.dump(objs, f, indent=4)
     except:
         print("Falhou em entender a foto!")
         return
 
 #Alterar imgQuant conforme numero de frames
-imgQuant = 6426
+imgQuant = 793
 
 #Alterar conforme quantidade de dados de heart rate. No caso: 803 = 6426/8
-dataQuant = 803
+dataQuant = 1
 for i in range(1, imgQuant + 1, dataQuant):
     imgName = 'frame' + str(i) + '.jpg'
     dataName = 'data' + str(i) + '.json'
-    faceAnalyze(imgName, dataName)
+    faceAnalyze(imgName, dataName, i)
