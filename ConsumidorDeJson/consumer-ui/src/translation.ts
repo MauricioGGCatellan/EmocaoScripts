3e0d5a4b1838bb0ff28cd92011c9ec0c5c2ce495
Maurício Gabriel Garcia Catellan <mauriciocatellan@gmail.com>
2026-04-24 11:44:09 -0300

    Tradução (faltou aa)

---
+++ ConsumidorDeJson/consumer-ui/src/translation.ts
@@ -0,0 +1,9 @@
+export const emotionTranslations: Record<string, string> = {
+    "fear": "medo",
+    "sad": "triste",
+    "neutral": "neutro",
+    "happy": "feliz",
+    "surprise": "surpresa",
+    "disgust": "nojo",
+    "angry": "raiva"
+}
